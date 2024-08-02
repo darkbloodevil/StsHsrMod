b4900a6d60f4bc90daaefd31cb31935e62a0610f
darkbloodevil <evilwithdarkblood@gmail.com>
2024-08-02 18:02:39 +0800

    设定random种子

--- src/main/java/HsrMod/util/RandomSelection.java
+++ src/main/java/HsrMod/util/RandomSelection.java
@@ -6,23 +6,27 @@ package HsrMod.util;
  * @description
  */
 
+import com.megacrit.cardcrawl.core.Settings;
+
 import java.util.Random;
 
-public class RandomSelection {
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.floorNum;
 
+public class RandomSelection {
+    public static Random selectionRandomRng;
     public static int[] selectRandomElements(int m, int n) {
         int[] array = new int[m];
         for (int i = 0; i < m; i++) {
             array[i] = i;
         }
 
-        Random random = new Random();
         int[] result = new int[n];
         int arrayLength = array.length;
 
         for (int i = 0; i < n; i++) {
+            selectionRandomRng = new Random(Settings.seed + floorNum);
             // 从剩余的元素中随机选择一个
-            int randomIndex = random.nextInt(arrayLength - i);
+            int randomIndex = selectionRandomRng.nextInt(arrayLength - i);
             result[i] = array[randomIndex];
 
             // 将已选择的元素移动到数组末尾（逻辑上删除）
